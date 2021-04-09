ddae86bdd740aa503829a6783c8a263d89936ca4
Taylor Hockley <taylor.hockleys@gmail.com>
2021-04-09 17:05:11 +0000

    ESLint program

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -6,40 +6,40 @@ const readline = require('readline-sync');
 const VALID_CHOICES = ['rock','paper','scissor'];
 
 function prompt(msg) {
-    console.log(`===>${msg}`);
+  console.log(`===>${msg}`);
 }
 function displayWinner(choice, computerChoice) {
-    prompt(`Your choice: ${choice}. Computers chose: ${computerChoice}`);
+  prompt(`Your choice: ${choice}. Computers chose: ${computerChoice}`);
 
-if(choice==='rock' && computerChoice==='scissor'){
+  if (choice === 'rock' && computerChoice === 'scissor') {
     prompt(` You Win!!!!`);
-} else if(choice==='scissor' && computerChoice==='paper'){
+  }     else if (choice === 'scissor' && computerChoice === 'paper') {
     prompt(` You Win!!!!`);
-} else if(choice==='paper' && computerChoice==='rock'){
+  }     else if (choice === 'paper' && computerChoice === 'rock') {
     prompt(` You Win!!!!`);
-} else if(choice===computerChoice){
+  }   else if (choice === computerChoice) {
     prompt(` Its a tie!`);
-}else {
+  }  else {
     prompt(` Computer Wins >:)`);
-}
+  }
 }
 
 
 while (true) {
-prompt(` Choose one: ${VALID_CHOICES.join(', ')}.`);
-let choice = readline.question();
+  prompt(` Choose one: ${VALID_CHOICES.join(', ')}.`);
+  let choice = readline.question();
 
-while(!VALID_CHOICES.includes(choice)) {
+  while (!VALID_CHOICES.includes(choice)) {
     prompt(`  That is not a valid option!`);
     choice = readline.question();
-}
+  }
 
-let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-let computerChoice = VALID_CHOICES[randomIndex];
-displayWinner(choice,computerChoice );
+  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+  let computerChoice = VALID_CHOICES[randomIndex];
+  displayWinner(choice,computerChoice );
 
 
-prompt('Do you want to play again (y/n)?');
+  prompt('Do you want to play again (y/n)?');
   let answer = readline.question().toLowerCase();
   while (answer[0] !== 'n' && answer[0] !== 'y') {
     prompt('Please enter "y" or "n".');
@@ -51,5 +51,3 @@ prompt('Do you want to play again (y/n)?');
 }
 
 
-
- 
